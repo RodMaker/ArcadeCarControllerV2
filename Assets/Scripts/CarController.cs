9adb19e0a416f075df9c1630572f6965ca80a8da
RodMaker <83619630+RodMaker@users.noreply.github.com>
2022-09-04 22:29:56 +0100

    initial commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    // get input
+    // use input to move sphere
+
+    public Rigidbody sphereRB;
+    public Rigidbody carRB;
+
+    public float fwdSpeed;
+    public float revSpeed;
+    public float turnSpeed;
+    public LayerMask groundLayer;
+
+    private float moveInput;
+    private float turnInput;
+    private bool isCarGrounded;
+
+    private float normalDrag;
+    public float modifiedDrag;
+    
+    public float alignToGroundTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // detach sphere from car
+        sphereRB.transform.parent = null;
+        carRB.transform.parent = null;
+
+        normalDrag = sphereRB.drag;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get input
+        moveInput = Input.GetAxisRaw("Vertical");
+        turnInput = Input.GetAxisRaw("Horizontal");
+
+        // calculate turning rotation
+        float newRotation = turnInput * turnSpeed * Time.deltaTime * moveInput;
+        
+        if (isCarGrounded)
+            transform.Rotate(0, newRotation, 0, Space.World);
+
+        // set cars position to sphere
+        transform.position = sphereRB.transform.position;
+
+        // raycast ground check
+        RaycastHit hit;
+        isCarGrounded = Physics.Raycast(transform.position, -transform.up, out hit, 1f, groundLayer);
+
+        // rotate car to allign with ground
+        Quaternion toRotateTo = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, toRotateTo, alignToGroundTime * Time.deltaTime);
+
+        // calculate movement direction
+        moveInput *= moveInput > 0 ? fwdSpeed : revSpeed;
+
+        // calculate drag
+        sphereRB.drag = isCarGrounded ? normalDrag : modifiedDrag;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isCarGrounded)
+        {
+            // move car
+            sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+        }
+        else
+        {
+            // add extra gravity
+            sphereRB.AddForce(transform.up * -40f);
+        }
+        carRB.MoveRotation(transform.rotation);
+    }
+}
---
+++ Assets/Scripts/WheelController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    public GameObject[] wheelsToRotate;
+    public TrailRenderer[] trails;
+    
+    public float rotationSpeed;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalAxis = Input.GetAxisRaw("Vertical");
+        float horizontalAxis = Input.GetAxisRaw("Horizontal");
+
+        foreach (var wheel in wheelsToRotate)
+        {
+            wheel.transform.Rotate(Time.deltaTime * verticalAxis * rotationSpeed, 0, 0, Space.Self);
+        }
+
+        if (horizontalAxis > 0)
+        {
+            // turning right
+            anim.SetBool("goingLeft", false);
+            anim.SetBool("goingRight", true);
+        }
+        else if (horizontalAxis < 0)
+        {
+            // turning left
+            anim.SetBool("goingRight", false);
+            anim.SetBool("goingLeft", true);
+        }
+        else
+        {
+            // going straight
+            anim.SetBool("goingLeft", false);
+            anim.SetBool("goingRight", false);
+        }
+
+        if (horizontalAxis != 0)
+        {
+            foreach (var trail in trails)
+            {
+                trail.emitting = true;
+            }
+        }
+        else
+        {
+            foreach (var trail in trails)
+            {
+                trail.emitting = false;
+            }
+        }
+    }
+}
