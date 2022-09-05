c9e17d57d06378e5c3b3ca7665bcffa948cdf9d1
RodMaker <83619630+RodMaker@users.noreply.github.com>
2022-09-05 17:29:16 +0100

    version050922

---
+++ Assets/Scripts/CollisionEffects.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionEffects : MonoBehaviour
+{
+    public GameObject hitEffectPrefab;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Instantiate(hitEffectPrefab, other.GetContact(0).point, Quaternion.identity);
+    }
+}
